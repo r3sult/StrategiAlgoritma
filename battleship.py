58208fed92083c91cee7438c3a6c8b5fbdac1a19
Albert Sahala <32014975+albertsahala@users.noreply.github.com>
2018-02-03 20:19:03 +0700

    Add files via upload

---
+++ battleship.py
@@ -0,0 +1,5 @@
+#       Anggota Kelompok : Thareq Muhammad Yusuf (13516004)
+#                          Albert Sahala Theodore (13516022)
+#                          Dandy Arif Rahman (13516086)
+
+#       Deskripsi        : Tugas Besar 1 Stima "Battleship"
